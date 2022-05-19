8140e76102bf8bb4063519cce069b3db0dbd0657
Patricia Namoro <patriciamae.namoro.pixel8@gmail.com>
2022-05-19 21:52:54 +0800

    first commit

---
+++ database/migrations/2022_05_19_070851_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->string('quantity');
+            $table->longText('description');
+            $table->string('skill_area');
+            $table->string('manufacturer');
+            $table->string('model');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
